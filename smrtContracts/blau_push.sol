8efc349583de3fae87746b0cdd2f7ff25907acd8
abu <abu@blockaudit.net>
2018-06-28 14:30:46 -0700

    push final

--- smrtContracts/blau_push.sol
+++ smrtContracts/blau_push.sol
@@ -1,1 +1,25 @@
-Sole
+pragma solidity ^0.4.2;
+
+contract testData 
+{
+	string constant public poster = "BlockAudit.net";
+
+	string constant public contractType = "Housing Deed Public";
+
+	string constant public pingVersion = "v0-1-0//a";
+
+	string constant public countyZip = "87654";
+
+	string constant public saleFrom = "First Name adn Last Name of saleFrom";
+
+	string constant public saleTo = "First Name adn Last Name of saleTo";
+
+	string constant public bankAllowed = "USA BANK OF USA";
+
+	string constant public inpectionComp = "Inside looks LLC";
+	
+	string constant public salePrice = "$1,000,000.00";
+
+
+
+}
